1422b4cffce9055dcd733ccb9001b7e66c5ac659
n.a.esteban.borquez <nicolas.esteban@melia.com>
2023-05-07 07:38:41 +0200

    Chapter 3 ready

---
+++ multiplication/src/main/java/com/nicoesteban/multiplication/challenge/ChallengeAttemptController.java
@@ -0,0 +1,34 @@
+package com.nicoesteban.multiplication.challenge;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * This class provides a REST API to POST the attempts from users.
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/attempts")
+public class ChallengeAttemptController {
+
+    private final ChallengeService challengeService;
+
+    @PostMapping
+    ResponseEntity<ChallengeAttempt> postResult(@RequestBody @Valid ChallengeAttemptDTO challengeAttemptDTO) {
+        return ResponseEntity.ok(challengeService.verifyAttempt(challengeAttemptDTO));
+    }
+
+    /*@GetMapping
+    ResponseEntity<List<ChallengeAttempt>> getStatics(@RequestParam("alias") String alias){
+        return ResponseEntity.ok(
+                challengeService.getStatsForUser(alias)
+        );
+    }*/
+
+}
---
+++ multiplication/src/main/java/com/nicoesteban/multiplication/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.nicoesteban.multiplication.user;
+/*
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByAlias(String alias);
+
+}
+*/
